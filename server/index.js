d979f7df2ec8e7672bac36877c0703d3818df9f9
ritikshaingle-9 <ritikshaingle.21@gmail.com>
2025-04-21 20:34:48 +0530

    database connected

--- server/index.js
+++ server/index.js
@@ -1,9 +1,21 @@
 import express from 'express';
 import cors from "cors";
+import mongoose from 'mongoose';
+import dotenv from "dotenv";
+dotenv.config();
 
  const app = express();
  app.use(express.json());
  app.use(cors());
+
+ const connectDB = async()=>{
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+
+    if (conn){
+    console.log("MongoDB connected successfully");
+    }
+};
+
 
  app.get("/health",(req,res)=>{
     res.status(200).json({
@@ -11,7 +23,8 @@ import cors from "cors";
     })
  });
 
-const PORT = 5002;
+const PORT = process.env.PORT || 5002;
 app.listen(PORT,()=>{
     console.log('server is running on port ${PORT}');
+    connectDB()
 });
